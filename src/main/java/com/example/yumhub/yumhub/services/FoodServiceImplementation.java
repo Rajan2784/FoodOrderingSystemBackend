8032b3694e41b9a663c792d10807b49862be685b
Rajan2784 <rajansingh6616@gmail.com>
2025-01-25 20:16:05 +0530

    Food upload feature created

--- src/main/java/com/example/yumhub/yumhub/services/FoodServiceImplementation.java
+++ src/main/java/com/example/yumhub/yumhub/services/FoodServiceImplementation.java
@@ -8,6 +8,28 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
-public class FoodServiceImplementation{
+public class FoodServiceImplementation implements FoodService{
+    @Autowired
+    private FoodRepository foodRepository;
 
+    @Override
+    public String addFoods(FoodDTO foodDTO) {
+        System.out.println("FoodDTO: " + foodDTO);
+        Foods foods = new Foods();
+        foods.setFoodCategory(foodDTO.getFoodCategory());
+        foods.setFoodDescription(foodDTO.getFoodDescription());
+        foods.setFoodImage(foodDTO.getFoodImage());
+        foods.setFoodName(foodDTO.getFoodName());
+        foods.setFoodStatus(foodDTO.getFoodStatus());
+        foods.setFoodType(foodDTO.getFoodType());
+        foods.setPrice(foodDTO.getPrice());
+        foods.setDiscount(foodDTO.getDiscount());
+        foodRepository.save(foods);
+        return "Food Details Uploaded!";
+    }
+
+    @Override
+    public List<Foods> getAllFoods() {
+        return List.of();
+    }
 }
